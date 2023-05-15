a472f9e3963539568b6fde2acf187c154d495420
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-15 11:55:47 +0100

    Create MockERC20.sol

---
+++ src/MockERC20.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor(
+        string name,
+        string symbol,
+        uint initalSupply
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, initalSupply)
+    }
+}
