7a564c16d40dc76f9ec264249f1a23cfa69cf31c
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-15 16:13:43 +0100

    Update MockChainlinkAggregator.sol

---
+++ src/MockChainlinkAggregator.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import "chainlink-contracts/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract MockChainlinkAggregator is AggregatorV3Interface {
+    int private price;
+    uint80 private roundId;
+
+    constructor(int _price) {
+        price = _price;
+        roundId = 1;
+    }
+
+    function decimals() external pure override returns (uint8) {
+        return 8;
+    }
+}
