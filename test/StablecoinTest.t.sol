fc23b10130c72e497879fa27ccbf5153e02d6965
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-15 21:06:36 +0100

    Update StablecoinTest.t.sol

--- test/StablecoinTest.t.sol
+++ test/StablecoinTest.t.sol
@@ -1,2 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../src/NGNStablecoin.sol";
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import "../src/MockChainlinkAggregator.sol";
+import "../src/MockERC20.sol";
+import "openzeppelin-contracts/utils/math/SafeMath.sol";
