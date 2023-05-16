4075f6e8965e76d7adebf2b2e4ef18269ad29e7f
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-16 09:58:24 +0100

    Update StablecoinTest.t.sol

--- test/StablecoinTest.t.sol
+++ test/StablecoinTest.t.sol
@@ -7,3 +7,8 @@ import "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import "../src/MockChainlinkAggregator.sol";
 import "../src/MockERC20.sol";
 import "openzeppelin-contracts/utils/math/SafeMath.sol";
+
+contract StablecoinTest is Test {
+    using SafeMath for uint256;
+    NGNStablecoin stablecoin;
+}
